e43bfd9d7da03984a06c0aa8d564cd9dcb7490ba
Eric Araya <earaya.mza@gmail.com>
2022-07-16 18:16:33 -0300

    cambios

--- js/main.js
+++
@@ -1,7 +0,0 @@
-let ingresarNumero = parseInt(prompt("Ingresar Numero"));
-
-for (let i = 2 ; i<=20; i++){
-    let resultado = ingresarNumero * 2;
-    alert( ingresarNumero +" X " + i + " = " +resultado );
-    break;
-}
