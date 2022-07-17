621cfb48eb3b3f3e707553a9acbbb25d4a77f563
Eric Araya <earaya.mza@gmail.com>
2022-07-16 21:49:27 -0300

    nuevos ejemplos js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
-let nombre = "Eric";
+/* let nombre = "Eric";
 console.log(nombre);
 let altura = 1.73
 let peso = 77
 let imc = peso / (altura*altura)
-console.log(imc)
+console.log(imc) */
+
+//Prompt para Textos
+
+/* let ingreseNombre = prompt("Ingrese su Nombre")
+alert("El nombre ingresado es:" + " " + ingreseNombre)
+console.log("El nombre ingresado es:" + " " + ingreseNombre) */
+
+
+//Calculadora IMC
+
+let peso = prompt("Ingrese su Peso");
+let altura = prompt("Ingrese su Altura");
+let imc = peso/ (altura*altura);
+let imcFinal = imc.toFixed(2); //tofixed sirve para achicar el numero de decimales que queremos ver o mostrar
+alert ("Su indice de masa corporal (IMC) es: "+ imcFinal + "%" );
+console.log( "Su indice de masa corporal (IMC) es: "+ imcFinal + "%" );
+
+//Prompt para Numeros
+
+/* let valorIngresado = parseInt(prompt("Ingrese un Valor:"));// convierte de string a integer
+let calculo = valorIngresado * 1.30;
+let mensaje = "El valor ingresado es: $" + calculo;
+alert(mensaje);
+console.log(mensaje)  */
